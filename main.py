af9bbbcbb9af4f42f4b038400ecccfb8479513c8
Yeison Cardona <yencardonaal@unal.edu.co>
2023-12-03 16:26:04 -0500

    First site update

---
+++ main.py
@@ -0,0 +1,39 @@
+from radiant.framework.server import RadiantAPI, RadiantServer, render
+from browser import html
+from browser.template import Template
+
+
+########################################################################
+class StaticApp(RadiantAPI):
+
+    # ----------------------------------------------------------------------
+    def __init__(self, *args, **kwargs):
+        """"""
+        super().__init__(*args, **kwargs)
+        self.body <= html.H1('Radiant-Framework')
+
+        self.body <= self.main()
+
+        # Template("menu").render()
+
+    # ----------------------------------------------------------------------
+    def main(self):
+        """"""
+        context = {
+            'title': 'TITULO',
+            'titles': ["Rendered",
+                       "Radiant",
+                       "Framework",
+                       "Layout",
+                       ],
+        }
+
+        return render('main.html', context)
+
+
+if __name__ == '__main__':
+    RadiantServer('StaticApp',
+                  port='5555',
+                  template='layout.html',
+                  static_app=True,
+                  )
